542e643d2575f46e7ad845a3fc55fdf0f8575c75
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2020-08-03 19:54:46 +0300

    Information systems
    Information systems was created

---
+++ BinarySearchTree/Information_systems.h
@@ -0,0 +1,13 @@
+#pragma once
+class information_systems {
+public:
+	void add();
+	void Delete();
+	void Count();
+	void menu();
+	void Print();
+	void List();
+	void Sum();
+	void Find();
+	void Aort();
+};
---
+++ BinarySearchTree/main.cpp
@@ -0,0 +1,14 @@
+/**
+* @author		Onur Akkepenek --> e-mail: akkepenek38@gmail.com
+* @date			2 Aralik 2019 Pazartesi
+* @brief		Bu kod parcacigi,agac yapisini baz alinarak olusturulmus ogrenci not sistemi simülasyonudur.
+*/
+#include<iostream>
+using namespace std;
+#include"BTS.h"
+#include"Information_systems.h"
+int main() {
+	information_systems BS;
+	BS.menu();
+	system("pause");
+}
